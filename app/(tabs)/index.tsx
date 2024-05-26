0e1c141f84d0209b5c6bcbaba47b0a65dbd70263
Nitish Kumar <nitish.5.kumar@bt.com>
2024-05-26 22:58:27 +0530

    image svg added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { ImageBackground } from "expo-image";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -5,7 +6,12 @@ export default function JobRecuiter() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Hlere</Text>
+        <ImageBackground
+          source={{
+            uri: "https://raw.githubusercontent.com/nitishkr72/social_media_app/15a5b1f36603d784805f240ff0dc1a5c65d6b3f3/assets/images/Frame.svg",
+          }}
+          style={{ width: "100%", height: 380 }}
+        />
       </View>
     </SafeAreaView>
   );
